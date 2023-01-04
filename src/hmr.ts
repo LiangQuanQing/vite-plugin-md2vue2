01341702716d257730e1b5b315f59e544b2965a6
liangquanqing <8340002022@qq.com>
2023-01-04 12:03:24 +0800

    feat: support the hmr for component switching

--- src/hmr.ts
+++ src/hmr.ts
@@ -9,15 +9,19 @@ export const hotRelaodImportCode = `
 export const hotReloadCode = `
   var $MD_VUE2_MAP = Object.create(null);
   var $MD_VUE2_INSTALLED = false;
+  var $MD_VUE2_VUE = null;
   var __MD_VUE2_HMR_RUNTIME__ = Object.create(null);
 
   if (typeof window !== 'undefined') {
     window.__MD_VUE2_HMR_RUNTIME__ = __MD_VUE2_HMR_RUNTIME__;
   }
 
-  __MD_VUE2_HMR_RUNTIME__.install = function (vue, browserify) {
+  __MD_VUE2_HMR_RUNTIME__.install = function (vue) {
     if ($MD_VUE2_INSTALLED) return;
     $MD_VUE2_INSTALLED = true;
+    if (!$MD_VUE2_VUE) {
+      $MD_VUE2_VUE = vue;
+    }
   }
 
   __MD_VUE2_HMR_RUNTIME__.createRecord = function (id, vm) {
@@ -32,10 +36,12 @@ export const hotReloadCode = `
   }
 
   __MD_VUE2_HMR_RUNTIME__.rerender = tryWrap(function (id, options) {
-    const instances = $MD_VUE2_MAP[id];
+    var instances = $MD_VUE2_MAP[id];
     if (instances && instances.length) {
       instances.forEach(instance => {
+        var globalComponents = $MD_VUE2_VUE ? $MD_VUE2_VUE.options.components : {};
         instance._staticTrees = [];
+        instance.$options.components = { ...globalComponents, ...options.default.components }
         instance.$options.render = options.render;
         instance.$options.staticRenderFns = options.staticRenderFns;
         instance.$forceUpdate();
